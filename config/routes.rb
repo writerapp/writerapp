4068a78ed559dc51a05ea1fa2303418beeae9cec
yuya2413 <klx250cbr250@gmail.com>
2020-01-01 04:55:15 +0000

    Add writers/registrations#confirm

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :writers, controllers: {registrations: 'writers/registrations'}
+  devise_scope :writer do
+  	get 'writers/sign_up/confirm', to: 'writers/registrations#confirm', as: 'confirm_writer_registration'
+  end
   devise_for :admins
-  devise_for :writers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
