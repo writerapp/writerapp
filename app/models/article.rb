b1fe41f19ce39fc92ef175c1d80347ee94524cbf
yuya2413 <klx250cbr250@gmail.com>
2020-01-22 08:53:50 +0000

    Update status name of ariticle model after MTG

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
   accepted_nested_attributes_for :headings, allow_destroy: true
   has_one :feedback, dependent: :destroy
 
-  enum status: [:新規作成, :見出し編集中, :見出し承認待ち, :本文編集中, :本文承認待ち, :納品完了]
+  enum status: [:見出し作成中, :見出し申請中, :記事執筆中, :納品申請中, :納品完了]
   validates :title, length: { maximum: 35, message: 'は35文字以内にしてください' }
 
 end
