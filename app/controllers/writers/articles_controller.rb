775d4c21cc8219eaef9b48b07e7a3010de453677
yuya2413 <klx250cbr250@gmail.com>
2020-01-25 04:18:01 +0000

    Add writers/articles#create

--- app/controllers/writers/articles_controller.rb
+++ app/controllers/writers/articles_controller.rb
@@ -1,16 +1,34 @@
 class Writers::ArticlesController < ApplicationController
-  def keywords
+  #def keywords
+  #end
+
+  def create
+    @article = Article.new(article_params)
+    @article.writer_id = current_writer.id
+    @article.keyword_1 = '髪の毛' #変更予定, spreadsheetから引っ張ってくる
+    @article.keyword_2 = Time.new.min #変更予定, spreadsheetから引っ張ってくる
+    @article.unit_price = current_writer.unit_price
+    @article.save
+    redirect_to writers_articles_path
   end
 
   def index
+    @article = Article.new #削除予定
+    @articles = Article.where(writer_id: current_writer.id)
   end
 
-  def show
-  end
+  #def show
+  #end
 
-  def edit_headings
-  end
+  #def edit_headings
+  #end
 
-  def edit
+  #def edit
+  #end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:writer_id, :keyword_1, :keyword_2, :keyword_3, :status, :title, :reference_1, :reference_2, :reference_3, :reference_4, :reference_5, :unit_price)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :writer, optional: true
   has_many :headings, dependent: :destroy
-  accepted_nested_attributes_for :headings, allow_destroy: true
+  accepts_nested_attributes_for :headings, allow_destroy: true
   has_one :feedback, dependent: :destroy
 
   enum status: [:見出し作成中, :見出し申請中, :記事執筆中, :納品申請中, :納品完了]
